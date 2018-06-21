0599d19b9eb9b2b485b6f10a705b8e2ee08f4a65
Stephanie Bowles <sbowl001@fiu.edu>
2018-06-21 11:54:14 +0800

    Register from react working

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import axios from 'axios';
 
-const Register = props => {
-    const linkStyle = {
-        margin: 10
+export default class Register extends Component {
+    state = {
+        username: '',
+        race: '',
+        password: ''
     }
-return (
-    <div> 
-    Register
-    </div>
-)
-}
 
-export default Register;
+    inputChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:5500/api/auth/register', this.state)
+        .then(user =>{
+            console.log(user)
+        })
+        .catch( err => console.log(err))
+    }
+    render(){
+        return(
+            <div style= {{marginTop: 20}}>
+            <div>
+                Register
+            </div>
+            <form onSubmit = {this.submitHandler}>
+            <input name ="username" placeholder="Username" value={this.state.username} onChange={this.inputChange}/>
+            <input name="race" placeholder="Race" value={this.state.race} onChange={this.inputChange}/>
+            <input name="password" placeholder="Password" value={this.state.password} onChange={this.inputChange}/>
+            <button type="submit"> Submit </button>
+            </form>
+            </div>
+        )
+    }
+}
